272977d2dfc7aa68b015693150d37680c5a88330
Kunuk Nykjær <knyk@novonordisk.com>
2023-05-13 11:58:54 +0200

    add vuetify

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="d-flex align-center flex-column">
+    <v-icon>mdi-home</v-icon>
+    <v-card width="400">
+      <v-card-title>This is a title</v-card-title>
+      <v-card-subtitle>This is a subtitle</v-card-subtitle>
+      <v-card-text> This is content </v-card-text>
+    </v-card>
+    <v-btn class="mt-10">Hello Sass Changes</v-btn>
   </div>
 </template>
+
+<style lang="scss">
+@use './settings';
+</style>
